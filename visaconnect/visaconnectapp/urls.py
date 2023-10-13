7302e3dabdb13574b6d20a129a23a3e22d80afd2
Md Akhtaruzjaman <98809898+mdakhtaruzjaman@users.noreply.github.com>
2023-10-13 02:10:32 +0000

    added posts, logo, connected facebook and social icons

--- visaconnect/visaconnectapp/urls.py
+++ visaconnect/visaconnectapp/urls.py
@@ -3,3 +3,12 @@ from . import views
 urlpatterns = [
     path('', views.hi, name = 'home-page'),
 ]
+
+#(the below code is to get access to facebook page and it's posts)
+#from django.urls import path
+#from . import views
+
+#urlpatterns = [
+    # Add other URL patterns
+   # path('get-page-posts/', views.get_page_posts, name='get_page_posts'),
+#]
--- visaconnect/visaconnectapp/views.py
+++ visaconnect/visaconnectapp/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def hi(request): 
     return render(request, 'visaconnectapp/hi.html')
+
+#(the below code is to get access to facebook page and it's posts)
+#from django.http import JsonResponse
+#import facebook
+
+#def get_page_posts(request):
+#    user_access_token = 'EAAE2zU3OxP8BO3mCsAygRRFHCDZBGqxEDjqzKdNUpC2jTpKABvMq5uNqRHlqIpvMBwLQWsqgCFF5zoWc1PyZBmnJmRuWDbZCHisEzcCH4LsweiFD4XH7Pfe9sWX6z8bINijwAwCTxEOFOqneuSyW69GS7ey5OOv6RMp5wB70xs17zmop5wyZAeLq1KbbKT3HG13vrAd53fAquJtIXhxNbbU0zg4DQw3Ro1LkJ5pNlUqhVnWiOfebliR1lrKUqJz5HPnHQ1OgSCgZD'
+ #   page_id = '129027193627517'
+
+  #  graph = facebook.GraphAPI(access_token=user_access_token, version="v13.0")
+
+   # try:
+        # Get the page's posts
+    #    posts = graph.get_connections(id=page_id, connection_name='posts')
+        # You can now work with the 'posts' data
+     #   return JsonResponse({'posts': posts})
+    #except facebook.GraphAPIError as e:
+     #   return JsonResponse({'error': str(e)})
